11e84b6f3c503c12b6833882e21574be65923652
chr56 <_>
2022-10-02 19:18:42 +0800

    [gradle] update kotlin to 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val apgVersion = "7.2.1"
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.7.10"
 
     id("com.android.library") version apgVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
