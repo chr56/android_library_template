24685e11ad23a8fa4f7be2a5f1d9e3a06b589ad5
chr_56 <_>
2023-03-04 11:30:29 +0800

    [gradle] add `android material design` (template)

--- lib-template/build.gradle.kts
+++ lib-template/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
+    //implementation("com.google.android.material:material:1.8.0")
+
     //implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
 }
