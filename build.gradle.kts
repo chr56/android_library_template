5adccfbe5e6f6cabeb49945e54090f70353657b6
chr_56 <_>
2023-03-04 11:15:47 +0800

    [gradle] update kotlin to 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val apgVersion = "7.4.1"
-    val kotlinVersion = "1.7.10"
+    val kotlinVersion = "1.8.10"
 
     id("com.android.library") version apgVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
