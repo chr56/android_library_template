469c6d93e7962ef2dee2d950e92c5bf57cefbd80
chr56 <chr0056@gmail.com>
2023-10-10 18:15:49 +0800

    [dependencies] upgrade androidx.fragment to `1.6.1`

--- lib-template/build.gradle.kts
+++ lib-template/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.annotation:annotation:1.5.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
