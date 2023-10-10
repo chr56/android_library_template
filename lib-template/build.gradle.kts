4b4a48362cdb9f8fda03c4c83f7e8b49b3aef072
chr56 <chr0056@gmail.com>
2023-10-10 18:18:43 +0800

    [dependencies] upgrade material to `1.9.0`

--- lib-template/build.gradle.kts
+++ lib-template/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
      implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
-    //implementation("com.google.android.material:material:1.8.0")
+    //implementation("com.google.android.material:material:1.9.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
