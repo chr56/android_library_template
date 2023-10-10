3d156e477920172ed7c4a94381ada2f26ac398ab
chr56 <chr0056@gmail.com>
2023-10-10 18:08:03 +0800

    [gradle] update kotlin to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val apgVersion = "7.4.2"
-    val kotlinVersion = "1.8.10"
+    val kotlinVersion = "1.9.10"
 
     id("com.android.library") version apgVersion apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
